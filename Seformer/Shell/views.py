ab746b43de3b2604b9de2b0b4a231082d5bf14ed
DarwinVL <lope803901@siigo.com>
2023-07-24 21:55:39 -0500

    first commit

---
+++ Seformer/Shell/views.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import request, response
+# Create your views here.
+def template(request):
+    return render(request,"index.html")
