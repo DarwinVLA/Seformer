db4a63dd005d2830b5b97305bed2c38937acce65
DarwinVL <lope803901@siigo.com>
2023-07-24 22:32:26 -0500

    ignore

--- Seformer/Shell/views.py
+++ Seformer/Shell/views.py
@@ -4,3 +4,5 @@ from django.http import request, response
 # Create your views here.
 def template(request):
     return render(request,"index.html")
+def template(request):
+    return render(request,"index.html")
